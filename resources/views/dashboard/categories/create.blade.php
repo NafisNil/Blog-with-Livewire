3a976488a19f2a4b70a8204c746af482066b6293
NafisNil <nafis.zaman1.an@gmail.com>
2022-09-22 00:30:25 +0600

    sep21_2

--- resources/views/dashboard/categories/create.blade.php
+++ resources/views/dashboard/categories/create.blade.php
@@ -1,14 +1,34 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Categories') }}
         </h2>
+    </x-slot>
+
+    <x-slot name="nav">
+            <div class="space-x-4">
+                <x-jet-nav-link href="{{ route('categories.index') }}" :active="request()->routeIs('categories.index')">
+                    {{ __('Index') }}
+                </x-jet-nav-link>
+
+                <x-jet-nav-link href="{{ route('categories.create') }}" :active="request()->routeIs('categories.create')">
+                    {{ __('Create') }}
+                </x-jet-nav-link>
+            </div>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+                <div class="p-6">
+                    <form action="{{route('categories.store')}}" method="post">
+                        @csrf
+                        <x-jet-label for="name" value="{{ __('Name') }}" />
+                        <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                        <span class="text-xs text-gray-400">Max (80 characters)</span>
+                        <x-jet-input-error for="name" class="mt-2"/>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
